640930d9b05860410e5758ee53b193d8c13a8d79
poxsilver4@gmail.com <poxsilver4@gmail.com>
2022-07-24 18:00:37 +0000

    fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ resource "google_project_service" "project_services" {
     "storage-component.googleapis.com",
     "containerregistry.googleapis.com",
     "cloudbuild.googleapis.com",
+    "iam.googleapis.com",
   ])
   service = each.key
   timeouts {
@@ -16,17 +17,30 @@ resource "google_project_service" "project_services" {
 }
 
 resource "google_storage_bucket" "bucket_3dmodels" {
-  name          = "${provider.google.project}-3dmodels"
-  location      = provider.google.region
+  name          = "${var.GOOGLE_CLOUD_PROJECT}-3dmodels"
+  location      = var.REGION
   force_destroy = true
   uniform_bucket_level_access = true
   depends_on = [google_project_service.project_services]
 }
 
-resource "google_storage_bucket" "bucket_3dmodels" {
-  name          = "${provider.google.project}-tfback"
-  location      = provider.google.region
+resource "google_storage_bucket" "bucket_tfback" {
+  name          = "${var.GOOGLE_CLOUD_PROJECT}-tfback"
+  location      = var.REGION
   force_destroy = true
   uniform_bucket_level_access = true
   depends_on = [google_project_service.project_services]
+}
+
+resource "google_service_account" "service_account_3dmodels" {
+  account_id   = "sa-3dmodels"
+  display_name = "sa-3dmodels"
+  depends_on = [google_project_service.project_services]
+}
+
+resource "null_resource" "service_account_key_json_download_1" {
+  provisioner "local-exec" {
+    command = "gcloud iam service-accounts keys create 'service-key.json' --iam-account='${google_service_account.service_account_3dmodels.email}' --project='${var.GOOGLE_CLOUD_PROJECT}';mv service-key.json ../"
+  }
+  depends_on = [google_service_account.service_account_3dmodels]
 }
